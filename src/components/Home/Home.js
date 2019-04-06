77b409b911e9c8409ddf84b47aadaf4ca855fd66
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-04-05 23:50:11 -0600

    Create Home component directories

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomeHeader from './HomeHeader/HomeHeader.js';
+
+class Home extends Component {
+	render() {
+		return (
+			<div className='home-container'>
+				<HomeHeader />
+			</div>
+		);
+	}
+}
+
+export default Home;
---
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import HeaderImg from 'images/header.png'
+
+class HomeHeader extends Component {
+	render() {
+		return (
+			<header className='home-header'>
+				<img className='header-img' src={  } />
+			</header>
+		);
+	}
+}
+
+export default HomeHeader;
