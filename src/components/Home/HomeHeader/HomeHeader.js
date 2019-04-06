dcc22cfe3b7e34957def146174bc226528b97522
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-04-06 02:51:03 -0600

    Add header img to HomeHeader component

--- src/components/Home/HomeHeader/HomeHeader.js
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
 import HeaderImg from 'images/header.png'
 
 class HomeHeader extends Component {
 	render() {
 		return (
 			<header className='home-header'>
-				<img className='header-img' src={  } />
+				<Row className='justify-content-sm-center'>
+					<Col sm='6'>
+						<img className='header-img' src={ HeaderImg } />
+					</Col>
+				</Row>
 			</header>
 		);
 	}
