f5cac28a23d7c5e81044c596e29db64224777a9a
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2019-04-06 02:50:38 -0600

    Add HomeSponsors reference to Home component file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import HomeHeader from './HomeHeader/HomeHeader.js';
+import HomeSponsors from './HomeSponsors/HomeSponsors.js';
+import sponsorsData from 'data/sponsors.json';
 
 class Home extends Component {
 	render() {
 		return (
 			<div className='home-container'>
 				<HomeHeader />
+				<HomeSponsors sponsors={ sponsorsData.sponsors } />
 			</div>
 		);
 	}
