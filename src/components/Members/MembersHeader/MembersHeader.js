b96c62c36daad74dd8cb139b0c6dd432d266e468
Aurora Tijerina <auro.tj@gmail.com>
2020-02-26 13:07:05 -0600

    Added responsivity to banner text

--- src/components/Members/MembersHeader/MembersHeader.js
+++ src/components/Members/MembersHeader/MembersHeader.js
@@ -5,13 +5,13 @@ class MembersHeader extends Component {
 		return(
 			<div className='members-header'>
 				<div className='container-legend'>
-					<h2>
+					<h2 className='title-text-banner'>
 						Members
-					</h2>
+					</h2 >
 					<div className='main-text-members'>
-						<br></br>
+						RoBorrego’s community is made by students with different skills in robotics, logistics and networking, all joined with a passion for exploring new technologies and sharing their knowledge with everybody.
 						<p className='main-text-members-emphasis'>
-							Founded in 2015 with only 4 members, we are now a team of 45 full-time students of different semesters and careers, with 2 professors of TI at Tecnológico de Monterrey.
+							Go ahead and meet the team!
 						</p>
 					</div>
 				</div>
