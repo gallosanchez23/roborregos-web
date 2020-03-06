505ed26b5234c6873792047b161eb4971d4e8d33
Aurora Tijerina <auro.tj@gmail.com>
2020-03-06 16:08:01 -0600

    Adjusted some code according to the standard followed.

--- src/components/Home/HomeInformation/HomeInformation.js
+++ src/components/Home/HomeInformation/HomeInformation.js
@@ -9,11 +9,11 @@ class HomeHeader extends Component {
 					<Col xs='10' md='6' className='information-col'>
 						<span>
 							<h3>
-							Robotics Team in Competition
+								Robotics Team in Competition
 							</h3>
 							<br/>
 							<p>
-							We  participate in different national and international competitions for autonomous robots such as Mexico's TMR (Torneo Mexicano de Robótica), RoboCup, and IEEE LARC (Latin American Robotics Competition). As a team, we want to demonstrate the potential of Mexico in the development and innovation of technology.
+								We  participate in different national and international competitions for autonomous robots such as Mexico's TMR (Torneo Mexicano de Robótica), RoboCup, and IEEE LARC (Latin American Robotics Competition). As a team, we want to demonstrate the potential of Mexico in the development and innovation of technology.
 							</p>
 						</span>
 					</Col>
@@ -22,11 +22,11 @@ class HomeHeader extends Component {
 					<Col xs='10' md={{span: 6, offset: 6}} className='information-col'>
 						<span>
 							<h3>
-							Social Projects
+								Social Projects
 							</h3>
 							<br/>
 							<p>
-							We like to share everything we’ve learned with the community, giving free classes, workshops and participating in webinars where we can talk and teach about all the technologies we’ve used and all the experiences we’ve had to try inspiring people to collaborate and develop technology.
+								We like to share everything we’ve learned with the community, giving free classes, workshops and participating in webinars where we can talk and teach about all the technologies we’ve used and all the experiences we’ve had to try inspiring people to collaborate and develop technology.
 							</p>
 						</span>
 					</Col>
@@ -35,11 +35,11 @@ class HomeHeader extends Component {
 					<Col xs='10' md='6' className='information-col'>
 						<span>
 							<h3>
-							Events and outreach
+								Events and outreach
 							</h3>
 							<br/>
 							<p>
-							We participate in congresses and events such as INCMty, Conexión Tec, The International Congress of Mechatronics - Automatization and Technology, Semana i and many more.
+								We participate in congresses and events such as INCMty, Conexión Tec, The International Congress of Mechatronics - Automatization and Technology, Semana i and many more.
 							</p>
 						</span>
 					</Col>
@@ -48,11 +48,11 @@ class HomeHeader extends Component {
 					<Col xs='10' md={{span: 6, offset: 6}} className='information-col'>
 						<span>
 							<h3>
-							Student community
+								Student community
 							</h3>
 							<br/>
 							<p>
-							To reach our community we give free workshops about useful technologies such as ROS, Git and Machine Learning, as well as our annually biggest event: Candidates, where the team gives weekly classes of basic programming, mechanics and electronics for anyone in the university interested, and create a robotics tournament to get new members.
+								To reach our community we give free workshops about useful technologies such as ROS, Git and Machine Learning, as well as our annually biggest event: Candidates, where the team gives weekly classes of basic programming, mechanics and electronics for anyone in the university interested, and create a robotics tournament to get new members.
 							</p>
 						</span>
 					</Col>
