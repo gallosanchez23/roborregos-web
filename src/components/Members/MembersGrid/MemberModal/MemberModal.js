c09368a55ed4fcae83e5bb6d74f3e76e6950a09a
Aurora Tijerina <auro.tj@gmail.com>
2020-03-21 23:24:28 -0600

    Added design for large and small views

--- src/components/Members/MembersGrid/MemberModal/MemberModal.js
+++ src/components/Members/MembersGrid/MemberModal/MemberModal.js
@@ -6,6 +6,7 @@ import CloseIcon from '@material-ui/icons/Close';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 import placeholder from 'images/placeholder-rectangle.png';
+import { MEDIUM_WIDTH } from 'constants.js';
 
 class MemberModal extends Component {
 	constructor(props) {
@@ -13,9 +14,16 @@ class MemberModal extends Component {
 
 		this.tryRequire = this.tryRequire.bind(this);
 		this.memberFullName = this.memberFullName.bind(this);
+		this.largeView = this.largeView.bind(this);
+		this.smallView = this.smallView.bind(this);
+		this.updateSizeView = this.updateSizeView.bind(this);
 
 		this.member = props.member;
 		this.handleHideModal = props.onHide;
+
+		this.state = {
+			show_large: (window.innerWidth >= MEDIUM_WIDTH)?true:false,
+		}
 	}
 
 	tryRequire(img_path) {
@@ -30,12 +38,21 @@ class MemberModal extends Component {
 		return this.member.name + ' ' + this.member.lastname;
 	}
 
-	render() {
+	componentDidMount() {
+		window.addEventListener('resize', this.updateSizeView);
+	}
+
+	updateSizeView(){
+		this.setState({
+			show_large: (window.innerWidth >= MEDIUM_WIDTH)?true:false,
+		});
+	}
+
+	largeView() {
 		return (
 			<div className='member-modal-container'>
 				<div className='container-helper'>
 					<Row className='justify-content-sm-center main-modal-row'>
-
 						<Col xs='8' lg='3' className='image-col'>
 							<div className='image-cropper'>
 								<img
@@ -45,19 +62,22 @@ class MemberModal extends Component {
 								/>
 							</div>
 						</Col>
-
 						<Col xs='9' lg='5' className='information-col'>
 							<div className='information-container'>
 								<Row noGutters={ true }>
 									<Col xs='11'>
-										<h2>
-											{ this.memberFullName() }
-										</h2>
 									</Col>
 									<Col xs='1'>
 										<IconButton className='member-modal-btn' onClick={ this.handleHideModal }>
 											<CloseIcon />
 										</IconButton>
+									</Col>
+								</Row>
+								<Row noGutters={ true }>
+									<Col>
+										<h2>
+											{ this.memberFullName() }
+										</h2>
 									</Col>
 								</Row>
 
@@ -68,8 +88,8 @@ class MemberModal extends Component {
 									<br/>
 									<br/>
 									{ this.member.description }
-									<br/>
-									<br/>
+								</p>
+								<p>
 									<Button href={ this.member.github } className='member-modal-btn'>
 										<Icon className='fab fa-github fa-fw' />
 										<span className='member-username'>
@@ -79,11 +99,80 @@ class MemberModal extends Component {
 								</p>
 							</div>
 						</Col>
-
 					</Row>
 				</div>
 			</div>
 		);
+	}
+
+	smallView() {
+		return (
+			<div className='member-modal-container'>
+				<div className='container-helper'>
+					<Row className='main-modal-row'>
+						<div className='information-col'>
+							<Col xs={6} className='image-col'>
+								<div className='image-cropper'>
+									<img
+										className='modal-member-image'
+										src={ this.tryRequire(this.member.img) }
+										alt={ this.memberFullName() }
+									/>
+								</div>
+							</Col>
+							<Col xs={4}>
+								<Row noGutters={ true }>
+									<Col xs='11'>
+									</Col>
+									<Col xs='1'>
+										<IconButton className='member-modal-btn' onClick={ this.handleHideModal }>
+											<CloseIcon />
+										</IconButton>
+									</Col>
+								</Row>
+								<Row noGutters={ true }>
+									<Col>
+										<h2>
+											{ this.memberFullName() }
+										</h2>
+									</Col>
+								</Row>
+
+								<p>
+									<strong>
+										{ this.member.role }
+									</strong>
+								</p>
+								<p>
+									<Button href={ this.member.github } className='member-modal-btn'>
+										<Icon className='fab fa-github fa-fw' />
+										<span className='member-username'>
+											{ this.member.github_user }
+										</span>
+									</Button>
+								</p>
+							</Col>
+							<Col  xs={2}></Col>
+						</div>
+						<div className='description-small'>
+							<Col>
+								<p>
+									{this.member.description}
+								</p>
+							</Col>
+						</div>
+					</Row>
+				</div>
+			</div>
+		);
+	}
+
+	render() {
+		if(this.state.show_large){
+			return this.largeView();
+		}else{
+			return this.smallView();
+		}
 	}
 }
 
