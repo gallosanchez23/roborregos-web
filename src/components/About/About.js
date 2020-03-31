d4c539388a5676ca510a615ae6a764db73437984
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-03-30 20:02:57 -0600

    Fixes code style issues

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import Footer from 'components/Footer/Footer.js';
 import AboutHeader from './AboutHeader/AboutHeader.js'
 import AboutTimeline from './AboutTimeline/AboutTimeline.js'
+import Footer from 'components/Footer/Footer.js';
 import timelineData from 'data/timeline.json';
 
 class About extends Component {
 	render() {
 		return (
 			<div className='about-container'>
-					<AboutHeader/>
-					<AboutTimeline events={ timelineData.events }/>
-					<Footer/>
+				<AboutHeader/>
+				<AboutTimeline events={ timelineData.events }/>
+				<Footer/>
 			</div>
 		);
 	}
