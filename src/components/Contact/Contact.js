7b788a56d8f408f6ecab2e4ed454f65206a361e1
José Eduardo Sánchez Yépiz <gallo.sanchez23@gmail.com>
2020-03-31 03:21:21 -0600

    Fixes code style issues (#73)

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 class Contact extends Component {
 	render() {
-		document.title = 'RoBorregos | Contact'
-		
+		document.title = 'RoBorregos | Contact';
+
 		return (
 			<div className='contact-container'>
 				<h1>
