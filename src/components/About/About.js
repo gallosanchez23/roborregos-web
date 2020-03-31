9ceee66d6746374f29dd9ab2e0b9ddf3f380b6e0
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-03-31 11:43:54 -0600

    Creates new About style files and adds reference to components

---
+++ src/components/About/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import AboutTimeline from './AboutTimeline/AboutTimeline.js'
+import Footer from 'components/Footer/Footer.js';
+import timelineData from 'data/timeline.json';
+import './About.css';
+
+class About extends Component {
+  render() {
+    document.title = 'RoBorregos | About';
+
+    return (
+      <div className='about-container'>
+        <AboutTimeline events={ timelineData.events }/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default About;
