0a7c599504a253a9b5df7d684aba2a168bd1114f
Josecisneros001 <joseacisnerosm@gmail.com>
2020-04-12 23:26:22 -0500

    add HomeInformation

--- src/components/Home/HomeInformation/HomeInformation.js
+++ src/components/Home/HomeInformation/HomeInformation.js
@@ -1,160 +1,67 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import { MEDIUM_WIDTH } from 'constants.js';
 import './HomeInformation.css';
 
-class HomeHeader extends Component {
+class HomeInformation extends Component {
   constructor(props) {
     super(props);
-
-    this.largeView = this.largeView.bind(this);
-    this.smallView = this.smallView.bind(this);
-    this.updateWindowViewState = this.updateWindowViewState.bind(this);
-
-    this.state = {
-      large_view: (window.innerWidth >= MEDIUM_WIDTH) ? true : false,
-    }
   }
 
-  componentDidMount() {
-    window.addEventListener('resize', this.updateWindowViewState);
-  }
-
-  updateWindowViewState() {
-    this.setState({
-      large_view: (window.innerWidth >= MEDIUM_WIDTH) ? true : false,
-    });
-  }
-
-  largeView() {
-    return(
-      <div className='home-information-container'>
-        <Row>
-          <Col className='information-section information-col'>
-            <span>
-              <h3 className='information-title'>
-                High-Performance Team
-              </h3>
-              <p className='information-paragraph'>
-                We  participate in different national and international competitions for autonomous robots such as Mexico's TMR (Torneo Mexicano de Robótica), RoboCup, and IEEE LARC (Latin American Robotics Competition). As a team, want to demonstrate the potential of Mexico in the development and innovation of technology.
-              </p>
-            </span>
-          </Col>
-          <Col className='image-section image-section-right background-1'></Col>
-        </Row>
-        <Row>
-          <Col className='image-section image-section-left background-2'></Col>
-          <Col className='information-section information-col'>
-            <span>
-              <h3 className='information-title'>
-                Social Projects
-              </h3>
-              <p className='information-paragraph'>
-                We like to share everything we’ve learned with the community, giving free classes, workshops and participating in webinars where we can talk and teach about all the technologies we’ve used and all the experiences we’ve had that inspire us.
-              </p>
-            </span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='information-section information-col'>
-            <span>
-              <h3 className='information-title'>
-                Events and outreach
-              </h3>
-              <p className='information-paragraph'>
-                We participate in congresses and events such as INCMty, Conexión Tec, The International Congress of Mechatronics - Automatization and Technology, Semana i and many more.
-              </p>
-            </span>
-          </Col>
-          <Col className='image-section image-section-right background-3'></Col>
-        </Row>
-        <Row>
-          <Col className='image-section image-section-left background-4'></Col>
-          <Col className='information-section information-col'>
-            <span>
-              <h3 className='information-title'>
-                Student community
-              </h3>
-              <p className='information-paragraph'>
-                To reach our community, we give free workshops about useful technologies such as: ROS, Git and Machine Learning, as well as our annual biggest event: Candidates, where the team gives weekly classes of basic programming, mechanics and electronics for anyone in the university interested, and organize a robotics tournament to get new members.
-              </p>
-            </span>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-
-  smallView() {
-    return(
-      <div className='home-information-container'>
-        <Row>
-          <Col className='information-section information-col'>
-            <span>
-              <h3 className='information-title'>
-                High-Performance Team
-              </h3>
-              <p className='information-paragraph'>
-                We  participate in different national and international competitions for autonomous robots such as Mexico's TMR (Torneo Mexicano de Robótica), RoboCup, and IEEE LARC (Latin American Robotics Competition). As a team, want to demonstrate the potential of Mexico in the development and innovation of technology.
-              </p>
-            </span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='image-section image-section-right background-1'></Col>
-        </Row>
-        <Row>
-          <Col className='information-section information-col'>
-            <span>
-              <h3 className='information-title'>
-                Social Projects
-              </h3>
-              <p className='information-paragraph'>
-                We like to share everything we’ve learned with the community, giving free classes, workshops and participating in webinars where we can talk and teach about all the technologies we’ve used and all the experiences we’ve had that inspire us.
-              </p>
-            </span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='image-section image-section-left background-2'></Col>
-        </Row>
-        <Row>
-          <Col className='information-section information-col'>
-            <span>
-              <h3 className='information-title'>
-                Events and outreach
-              </h3>
-              <p className='information-paragraph'>
-                We participate in congresses and events such as INCMty, Conexión Tec, The International Congress of Mechatronics - Automatization and Technology, Semana i and many more.
-              </p>
-            </span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='image-section image-section-right background-3'></Col>
-        </Row>
-        <Row>
-          <Col className='information-section information-col'>
-            <span>
-              <h3 className='information-title'>
-                Student community
-              </h3>
-              <p className='information-paragraph'>
-                To reach our community, we give free workshops about useful technologies such as: ROS, Git and Machine Learning, as well as our annual biggest event: Candidates, where the team gives weekly classes of basic programming, mechanics and electronics for anyone in the university interested, and organize a robotics tournament to get new members.
-              </p>
-            </span>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='image-section image-section-left background-4'></Col>
-        </Row>
-      </div>
-    );
-  }
 
   render() {
-    return ((this.state.large_view) ? this.largeView() : this.smallView());
+    return (
+          <div className='home-information-container-all'>
+            <div className='home-information-container home-information-container-left home-information-container-competitions'>
+              <div className='home-information-circle'>
+              </div>
+              <div className='home-information-text'>
+                <div className='home-information-title'>
+                    High-Performance Team
+                </div>
+                <div className='home-information-simple'>
+                    We participate in different national and international competitions for autonomous robots such as Mexico's TMR (Torneo Mexicano de Robótica), RoboCup, and IEEE LARC (Latin American Robotics Competition). As a team, want to demonstrate the potential of Mexico in the development and innovation of technology.
+                </div>
+              </div>
+            </div>
+            <div className='home-information-container home-information-container-right home-information-container-social'>
+              <div className='home-information-circle'>
+              </div>
+              <div className='home-information-text'>
+                <div className='home-information-title'>
+                  Social Projects
+                </div>
+                <div className='home-information-simple'>
+                  We like to share everything we’ve learned with the community, giving free classes, workshops and participating in webinars where we can talk and teach about all the technologies we’ve used and all the experiences we’ve had that inspire us.
+                </div>
+              </div>
+            </div>
+            <div className='home-information-container home-information-container-left home-information-container-events'>
+              <div className='home-information-circle'>
+              </div>
+              <div className='home-information-text'>
+                <div className='home-information-title'>
+                  Events & Outreach
+                </div>
+                <div className='home-information-simple'>
+                  We participate in congresses and events such as INCMty, Conexión Tec, The International Congress of Mechatronics - Automatization and Technology, Semana i and many more.
+                </div>
+              </div>
+            </div>
+            <div className='home-information-container home-information-container-right home-information-container-students'>
+              <div className='home-information-circle'>
+              </div>
+              <div className='home-information-text'>
+                <div className='home-information-title'>
+                  Student Community
+                </div>
+                <div className='home-information-simple'>
+                To reach our community, we give free workshops about useful technologies such as: ROS, Git and Machine Learning, as well as our annual biggest event: Candidates, where the team gives weekly classes of basic programming, mechanics and electronics for anyone in the university interested, and organize a robotics tournament to get new members.
+                </div>
+              </div>
+            </div>
+          </div>
+    );
+
   }
 }
 
-export default HomeHeader;
+export default HomeInformation;
