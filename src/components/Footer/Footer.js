80b1057e2b4f499ea9e83f3d74b8b0d93097309c
Aurora Tijerina <auro.tj@gmail.com>
2020-04-27 02:30:29 -0500

    Updated design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,81 +1,108 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import logo from 'images/white_logo.png';
+import { MEDIUM_WIDTH, PHONE_SIZE } from 'constants.js';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import './Footer.css';
 
 function sitemapLink(link, legend) {
-  return (
-    <span>
-      <a href={ link } className='sitemap-link'>
-        { legend }
-      </a>
-      <br/>
-    </span>
-  );
+	return (
+		<span>
+			<a href={ link } className='sitemap-link'>
+				{ legend }
+			</a>
+			<br/>
+		</span>
+	);
 }
 
 function sitemapIconButton(link, icon) {
-  return (
-    <IconButton
-      component='a'
-      href={ link }
-      color='inherit'
-      className='sitemap-link'
-    >
-      { icon }
-    </IconButton>
-  );
+	return (
+		<IconButton
+		component='a'
+		href={ link }
+		color='inherit'
+		className='sitemap-link'
+		>
+			{ icon }
+		</IconButton>
+	);
 }
 
 class Footer extends Component {
-  render() {
-    return(
-      <div className='footer-container'>
-        <Container>
-          <Row className='justify-content-md-center'>
-            <Col xs='10' md='2'>
-              <img src={logo} className='footer-logo' alt='logo' />
-            </Col>
-            <Col xs='10' md='4'>
-              <h4 className='footer-title'>
-                RoBorregos
-              </h4>
-              <p>
-                <small>
-                  RoBorregos 2020 © All rights reserved | ITESM | Monterrey, Nuevo León, México
-                </small>
-              </p>
-            </Col>
-            <Col xs='10' md='3'>
-              <h4 className='footer-title'>
-                Site
-              </h4>
-              <p>
-                { sitemapLink('/', 'Home') }
-                { sitemapLink('/about', 'About') }
-                { sitemapLink('/members', 'Members') }
-                { sitemapLink('/contact', 'Contact') }
-              </p>
-            </Col>
-            <Col xs='10' md='3'>
-              <h4 className='footer-title'>
-                Social media
-              </h4>
-              <p>
-                { sitemapIconButton('https://github.com/RoBorregos/', <GitHubIcon fontSize='small' />) }
-                { sitemapIconButton('https://www.facebook.com/RoBorregos/', <FacebookIcon fontSize='small' />) }
-                { sitemapIconButton('https://www.instagram.com/roborregos/', <InstagramIcon fontSize='small' />) }
-              </p>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+
+		this.setIconSize = this.setIconSize.bind(this);
+
+		this.members = props.members;
+
+		this.state = {
+			icon_size: (window.innerWidth > MEDIUM_WIDTH)?30:(window.innerWidth > PHONE_SIZE)?20:10,
+		}
+	}
+
+	componentDidMount() {
+		window.addEventListener('resize', this.setIconSize);
+	}
+
+	setIconSize(){
+		this.setState({
+			icon_size : (window.innerWidth >= MEDIUM_WIDTH)?30:(window.innerWidth >= PHONE_SIZE)?20:10,
+		});
+	}
+
+	render() {
+		return(
+			<div className='footer-container'>
+				<Row className='footer-row'>
+					<Col lg='4' className='col-logo'>
+						<img src={logo} className='footer-logo' alt='logo' />
+					</Col>
+					<Col lg='4' className='footer-col'>
+						<div className='sitemap-link'>
+							{ sitemapLink('/', 'Home') }
+						</div>
+						<div className='sitemap-link'>
+							{ sitemapLink('/about', 'About') }
+						</div>
+						<div className='sitemap-link'>
+							{ sitemapLink('/members', 'Members') }
+						</div>
+						<div className='sitemap-link'>
+							{ sitemapLink('/contact', 'Contact') }
+						</div>
+					</Col>
+					<Col lg='4'>
+						<Row className='goback-button'>
+							<IconButton
+							component='a'
+							href={ '/' }
+							color='inherit'
+							className='sitemap-link'
+							>
+								<ExpandLessIcon/>
+								<div className='goback-text'>Back to top</div>
+							</IconButton>
+						</Row>
+						<Row className='row-socialMedia'>
+							<p>
+								{ sitemapIconButton('https://github.com/RoBorregos/', <GitHubIcon style={{ fontSize: this.state.icon_size }} />) }
+								{ sitemapIconButton('https://www.facebook.com/RoBorregos/', <FacebookIcon style={{ fontSize: this.state.icon_size }} />) }
+								{ sitemapIconButton('https://www.instagram.com/roborregos/', <InstagramIcon style={{ fontSize: this.state.icon_size }} />) }
+								@2020 RoBorregos
+							</p>
+						</Row>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
 export default Footer;
