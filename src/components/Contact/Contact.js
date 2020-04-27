645966076d6b34b958c3f4e11a3d145e0b004f2e
Josecisneros001 <joseacisnerosm@gmail.com>
2020-04-27 12:37:14 -0500

    initContact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import ContactJoinUs from './ContactJoinUs/ContactJoinUs.js';
+import ContactSponsorUs from './ContactSponsorUs/ContactSponsorUs.js';
+import positionsData from 'data/positions.json';
+import Footer from '../Footer/Footer';
 import './Contact.css';
 
 class Contact extends Component {
+  constructor(props){
+		super(props);
+	}
   render() {
     document.title = 'RoBorregos | Contact';
 
     return (
       <div className='contact-container'>
-        <h1>
-          Contact
-        </h1>
+        <ContactSponsorUs />
+        <ContactJoinUs positionsData={positionsData}/>
+        <Footer />
       </div>
     );
   }
