4ed95622f25abe4a1a855d4625e6040c4f323059
Josecisneros001 <joseacisnerosm@gmail.com>
2020-04-30 23:17:02 -0500

    Add SponsorUs

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ContactJoinUs from './ContactJoinUs/ContactJoinUs.js';
 import ContactSponsorUs from './ContactSponsorUs/ContactSponsorUs.js';
 import positionsData from 'data/positions.json';
+import packagesData from 'data/sponsors.json';
 import Footer from '../Footer/Footer';
 import './Contact.css';
 
@@ -14,7 +15,7 @@ class Contact extends Component {
 
     return (
       <div className='contact-container'>
-        <ContactSponsorUs />
+        <ContactSponsorUs packagesData={packagesData} />
         <ContactJoinUs positionsData={positionsData}/>
         <Footer />
       </div>
--- src/components/Contact/ContactSponsorUs/ContactSponsorUs.js
+++ src/components/Contact/ContactSponsorUs/ContactSponsorUs.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import ContactSponsorPackages from './ContactSponsorPackages/ContactSponsorPackages';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
 import './ContactSponsorUs.css';
 
 class ContactSponsorUs extends Component {
+	constructor(props){
+		super(props);
+		this.packagesData=props.packagesData;
+		this.sponsorUsCallback = this.sponsorUsCallback.bind(this);
+	}
+	sponsorUsCallback(){
+	
+	}
 	render() {
 		return(
-			<div className='contact-sponsorus'>
+			<div id='contact-sponsorus' className='contact-sponsorus'>
+				<div className='contact-sponsorus-title-container'>
+					<div className='contact-sponsorus-title-icon-container'>
+						<FontAwesomeIcon icon={faDollarSign}/>
+					</div>
+					<span>
+						Would you like to support RoBorregos? 
+						<br/>
+						Join our sponsors team!
+					</span>
+				</div>
+				<ContactSponsorPackages packagesData={this.packagesData}/>
+				<div className='contact-sponsorus-btn-container'>
+					<button onClick={this.sponsorUsCallback} className="contact-sponsorus-btn" variant='outline-primary'>
+						Sponsor Us!
+					</button>
+				</div>
 			</div>
 		);
 	}
 }
 
 export default ContactSponsorUs;
+
