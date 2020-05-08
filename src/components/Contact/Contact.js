f0ffc870eef053b6998462e041fe80cb254d1bad
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-05-08 18:28:59 -0500

    Modifies footer import

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ContactJoinUs from './ContactJoinUs/ContactJoinUs.js';
 import ContactSponsorUs from './ContactSponsorUs/ContactSponsorUs.js';
 import positionsData from 'data/positions.json';
-import Footer from '../Footer/Footer';
+import Footer from 'components/Footer/Footer';
 import './Contact.css';
 
 class Contact extends Component {
