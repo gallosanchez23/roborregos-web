b2f1c633fcc9bf1a4bf03880e344f5027297c1b1
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-05-08 19:02:44 -0500

    Fixes code style and indentation issues

--- src/components/Contact/ContactSponsorUs/ContactSponsorUs.js
+++ src/components/Contact/ContactSponsorUs/ContactSponsorUs.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import './ContactSponsorUs.css';
 
 class ContactSponsorUs extends Component {
-	render() {
-		return(
-			<div className='contact-sponsorus'>
-			</div>
-		);
-	}
+  render() {
+    return(
+      <div className='contact-sponsorus'></div>
+    );
+  }
 }
 
 export default ContactSponsorUs;
