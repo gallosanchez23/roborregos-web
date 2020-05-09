176bc9364a1a7c65168687d771b1b4c7e093fe10
Eduardo Sánchez <gallo.sanchez23@gmail.com>
2020-05-08 20:39:46 -0500

    Refactors ContactSponsorPackages component to simplify elements tree

--- src/components/Contact/ContactSponsorUs/ContactSponsorPackages/ContactSponsorPackages.js
+++ src/components/Contact/ContactSponsorUs/ContactSponsorPackages/ContactSponsorPackages.js
@@ -1,53 +1,61 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
-import logo from '../../../../images/white_logo.png';
+import { Row, Col } from 'react-bootstrap';
+import logo from 'images/white_logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import './ContactSponsorPackages.css';
 
 class ContactSponsorPackages extends Component {
-	constructor(props) {
-		super(props);
-		this.packages = props.packagesData.packages;
-	}
+  constructor(props) {
+    super(props);
 
-	render() {
-		return(
-			<div className='contact-sponsor-packages-grid'>
-						{this.packages.map(pkg =>(
-								<Card
-									className='contact-sponsor-packages-card'
-									key={ pkg.id }
-								>
-									<Card.Body>
-										<div className='contact-sponsor-packages-card-content'>
-											<div className='contact-sponsor-packages-title-container'>
-												<span>{pkg.name}</span>
-											</div>
-											{pkg.benefits.map(benefit =>(
-												<div className='contact-sponsor-packages-benefit-container'>
-													<div className='contact-sponsor-packages-benefit-icon'>
-														<FontAwesomeIcon icon={ faCheckCircle }/>
-													</div>
-													<div className='contact-sponsor-packages-benefit-text'>
-														{benefit}
-													</div>
-													
-												</div>
-											))}
-											<div className='contact-sponsor-packages-card-footer'>
-												<div className='contact-sponsor-packages-card-footer-whiteBar left'><div /></div>
-												<img src={logo} alt="Logo" />;
-												<div className='contact-sponsor-packages-card-footer-whiteBar right'><div /></div>
-											</div>
-										</div>
-									</Card.Body>
-									
-								</Card>
-						))}
-			</div>
-		);
-	}
+    this.packages = props.packagesData.packages;
+  }
+
+  render() {
+    return(
+      <Row className='justify-content-center'>
+        <Col xs='10' sm='10' md='10' lg='10' xl='10'>
+          <Row className='justify-content-center mt-4'>
+            { this.packages.map(pkg => (
+              <Col xs='10' sm='6' md='6' lg='3' xl='3' className='mt-4'>
+                <Card
+                  className='packages-card'
+                  key={ pkg.id }
+                >
+                  <Card.Body>
+                    <h4>
+                      { pkg.name }
+                    </h4>
+                    <hr />
+                    { pkg.benefits.map(benefit => (
+                      <div className='benefit-container'>
+                        <div className='benefit-icon'>
+                          <FontAwesomeIcon icon={ faCheckCircle } className='mr-2' />
+                        </div>
+                        <div className='benefit-text'>
+                          { benefit }
+                        </div>
+                      </div>
+                    )) }
+                    <Row className='justify-content-center mt-4 package-card-footer'>
+                      <Col xs='4' sm='4' md='4' lg='4' xl='4' className='no-padding'><hr /></Col>
+                      <Col xs='3' sm='3' md='3' lg='3' className='no-padding'>
+                        <img src={ logo } alt='Logo' />
+                      </Col>
+                      <Col xs='4' sm='4' md='4' lg='4' xl='4' className='no-padding'><hr /></Col>
+                    </Row>
+                  </Card.Body>
+
+                </Card>
+              </Col>
+            )) }
+          </Row>
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default ContactSponsorPackages;
