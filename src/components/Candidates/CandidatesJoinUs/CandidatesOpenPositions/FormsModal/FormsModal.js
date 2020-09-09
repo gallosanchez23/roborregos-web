e13425bf72470187b40cf4b4e3284e87df376698
Mauricio Juárez Ibáñez <44931871+zJuarez@users.noreply.github.com>
2020-09-09 18:19:36 -0500

    Update src/components/Candidates/CandidatesJoinUs/CandidatesOpenPositions/FormsModal/FormsModal.js
    Co-authored-by: Ricardo Chapa Romero <47112298+RicardoChapaRomero@users.noreply.github.com>

--- src/components/Candidates/CandidatesJoinUs/CandidatesOpenPositions/FormsModal/FormsModal.js
+++ src/components/Candidates/CandidatesJoinUs/CandidatesOpenPositions/FormsModal/FormsModal.js
@@ -83,7 +83,7 @@ class FormsModal extends Component {
                   <option value="6">6</option>
                   <option value="7">7</option>
                   <option value="8">8</option>
-                  <option value="9">9</option>
+                  <option value="9">9+</option>
                 </Input>
               </FormGroup>
 
