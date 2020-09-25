9eb48baf61b8c7b884e9998021bed925bac11433
Alexis Zugasti <alexiszugasti@gmail.com>
2020-09-25 18:21:50 -0500

    Add 'Join  the Team' button
    Add button under legacy section with legend 'Join the Team' that redirects to the candidates section.

--- src/components/Members/MembersJoinUs/MembersJoinUs.js
+++ src/components/Members/MembersJoinUs/MembersJoinUs.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './MembersJoinUs.css';
 
 class MembersJoinUs extends Component {
+  handleClick = () => {
+    window.location.href = 'candidates';
+    this.scrollToBottom();
+  }
+
   render() {
     return (
       <div className='members-join-us'>
-
+        <button 
+          onClick={this.handleClick} 
+          className='members-join-us-button members-join-us-button-contain members-join-us-button-text'>
+              Join the Team
+        </button>
       </div>
     );
   }
