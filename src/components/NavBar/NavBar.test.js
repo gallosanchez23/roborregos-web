4dd3fa56a61ca6f0eea7338a3f27636086aded7d
Tomas Lugo <lugo.lbmi@gmail.com>
2020-10-01 12:32:29 -0500

    Creates unit tests for NavBar - Creates a rendering test - Creates a clicking test for proper linking - Adds history package - Creates a setupFile for window.scrollTo

---
+++ src/components/NavBar/NavBar.test.js
@@ -0,0 +1,96 @@
+// @flow
+import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import { MemoryRouter, Router } from 'react-router-dom'
+import { fireEvent } from '@testing-library/react'
+import { createMemoryHistory } from 'history'
+import routesData from '../../data/routes.json'
+
+import NavBar from './NavBar'
+
+type RouteType = {
+  path: string,
+  legend: string,
+  component: string
+};
+
+let container = null
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div')
+  if (document.body != null) {
+    document.body.appendChild(container)
+  } else {
+    expect(document.body).not.toEqual(null)
+  }
+})
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container)
+  if (container != null) {
+    container.remove()
+  } else {
+    expect(container).not.toEqual(null)
+  }
+  container = null
+})
+
+it('<NavBar> Renders correctly', () => {
+  act(() => {
+    render(
+      <MemoryRouter>
+        <NavBar routes={routesData.routes} />
+      </MemoryRouter>, container,
+    )
+  })
+
+  routesData.routes.forEach((route: RouteType, index: number) => {
+    const current_button = document.querySelector(`[data-rb-event-key="${index}"]`)
+    if (current_button != null) {
+      const current_legend = current_button.children
+      expect(current_legend).toHaveLength(1)
+      expect(current_legend[0].textContent).toBe(route.legend)
+      expect(current_button.getAttribute('href')).toBe(route.path)
+
+      if (index === 0) {
+        expect(current_button.className).toContain('active')
+      } else {
+        expect(current_button.className).not.toContain('active')
+      }
+    } else {
+      expect(current_button).not.toEqual(null)
+    }
+  })
+})
+
+it('<NavBar> Links correctly when clicked', () => {
+  const history = createMemoryHistory()
+  act(() => {
+    render(
+      <Router history={history}>
+        <NavBar routes={routesData.routes} />
+      </Router>, container,
+    )
+  })
+
+  routesData.routes.forEach((route: RouteType, index: number) => {
+    const current_button = document.querySelector(`[data-rb-event-key="${index}"]`)
+    if (current_button != null) {
+      if (index !== 0) {
+        expect(current_button.className).not.toContain('active')
+      }
+
+      fireEvent(current_button, new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }))
+
+      expect(current_button.className).toContain('active')
+      expect(history.location.pathname).toBe(route.path)
+    } else {
+      expect(current_button).not.toEqual(null)
+    }
+  })
+})
