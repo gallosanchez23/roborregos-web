5ca68a8b16961a6dc62b92a8811d7840cd3acda5
Tomas Lugo <lugo.lbmi@gmail.com>
2020-10-07 11:22:17 -0500

    Fixes style issues in Contact Donations/Header

--- src/components/Contact/ContactHeader/ContactHeader.js
+++ src/components/Contact/ContactHeader/ContactHeader.js
@@ -1,35 +1,38 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import './ContactHeader.css';
+// @flow
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import './ContactHeader.css'
 
-class ContactHeader extends Component {
-  scrollToInfo(){
-    window.scrollBy(0, window.innerHeight - 48 - window.scrollY);
+type Props = {};
+
+class ContactHeader extends Component<Props> {
+  scrollToInfo = () => {
+    window.scrollBy(0, window.innerHeight - 48 - window.scrollY)
   }
 
   render() {
     return (
-      <div className='contact-header'>
-        <div className='container-legend'>
-          <h2 className='title-text-banner'>
+      <div className="contact-header">
+        <div className="container-legend">
+          <h2 className="title-text-banner">
             Contact
           </h2>
-          <div className='main-text-contact'>
+          <div className="main-text-contact">
             <p>
               Would you like to support RoBorregos? Join our sponsors team!
             </p>
           </div>
         </div>
-        <div className='contact-header-footer'>
+        <div className="contact-header-footer">
           <p>
             Learn More
           </p>
-          <FontAwesomeIcon onClick={ this.scrollToInfo } icon={ faAngleDown } className='icon-btn' />
+          <FontAwesomeIcon onClick={this.scrollToInfo} icon={faAngleDown} className="icon-btn" />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default ContactHeader;
+export default ContactHeader
