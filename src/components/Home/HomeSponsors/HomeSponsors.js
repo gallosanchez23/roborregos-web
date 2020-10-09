4574bfa7b46e69114b9fc00566ba5eed4f284173
Jamir <A00826275@itesm.mx>
2020-10-09 12:54:44 -0600

    Corrected home eslint flow errors

--- src/components/Home/HomeSponsors/HomeSponsors.js
+++ src/components/Home/HomeSponsors/HomeSponsors.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import HomeSingleSponsor from './HomeSingleSponsor/HomeSingleSponsor.js';
-import './HomeSponsors.css';
+import React, { Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import HomeSingleSponsor from './HomeSingleSponsor/HomeSingleSponsor'
+import './HomeSponsors.css'
 
 class HomeSponsors extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.sponsors = props.sponsors;
+    this.sponsors = props.sponsors
   }
 
   render() {
     return (
-      <div className='home-sponsors-container'>
-        <div className='container-helper'>
-          <Row className='justify-content-sm-center'>
-            <Col sm='10'>
+      <div className="home-sponsors-container">
+        <div className="container-helper">
+          <Row className="justify-content-sm-center">
+            <Col sm="10">
               <h3>
                 Sponsors
               </h3>
-              <Row className='justify-content-sm-center'>
-                {this.sponsors.map((sponsor, index) =>(
-                  <Col xs="6" sm='4' md='2' key={ index } className='sponsor-col' >
+              <Row className="justify-content-sm-center">
+                {this.sponsors.map((sponsor, index) => (
+                  <Col xs="6" sm="4" md="2" key={index} className="sponsor-col">
                     <HomeSingleSponsor
-                      sponsor={ sponsor }
+                      sponsor={sponsor}
                     />
                   </Col>
                 ))}
@@ -32,8 +32,8 @@ class HomeSponsors extends Component {
           </Row>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default HomeSponsors;
+export default HomeSponsors
