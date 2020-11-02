c1fc6c4d92e343c9b45d81e4582ba2e57f011e54
Jamir <A00826275@itesm.mx>
2020-11-02 08:57:27 -0700

    Eslint over About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import timelineData from '../../data/timeline.json'
 import Footer from '../Footer/Footer'
 import AboutHeader from './AboutHeader/AboutHeader'
 import AboutTimeline from './AboutTimeline/AboutTimeline'
 import './About.css'
 
-class About extends Component {
-  render() {
-    document.title = 'RoBorregos | About'
-
-    return (
-      <div className="about-container">
-        <AboutHeader />
-        <AboutTimeline events={timelineData.events} />
-        <Footer />
-      </div>
-    )
-  }
+const About = () => {
+  document.title = 'RoBorregos | About'
+  return (
+    <div className="about-container">
+      <AboutHeader />
+      <AboutTimeline events={timelineData.events} />
+      <Footer />
+    </div>
+  )
 }
 
 export default About
