430f5a903fb0655dbc2e50d133c7251ac2a570c6
Jamir <A00826275@itesm.mx>
2020-11-06 18:30:55 -0700

    added dependency that did not allowed to test about

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
 import { act } from 'react-dom/test-utils'
+import 'intersection-observer'
 
 import About from './About'
 
