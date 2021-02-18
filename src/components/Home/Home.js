672b76e022570f9cbadfef8651a8b6deffeee191
zJuarez <mji2000.741@gmail.com>
2021-02-17 18:34:57 -0600

    [home carousel v1] testing carousel on home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ import HomeMiniInformation from './HomeMiniInformation/HomeMiniInformation'
 import HomeSponsors from './HomeSponsors/HomeSponsors'
 import HomeMisionVision from './HomeMisionVision/HomeMisionVision'
 import sponsorsData from '../../data/sponsors.json'
+import HomeCarousel from './HomeCarousel/HomeCarousel'
 
 const Home = () => {
   document.title = 'RoBorregos | Home'
@@ -15,6 +16,7 @@ const Home = () => {
       <HomeHeader />
       <HomeMiniInformation />
       <HomeMisionVision />
+      <HomeCarousel />
       <HomeSponsors sponsors={sponsorsData.sponsors} />
     </>
   )
