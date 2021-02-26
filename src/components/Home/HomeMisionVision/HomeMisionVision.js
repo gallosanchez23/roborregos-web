7481724531ad6a164fb2e8d4952f458e14f5103e
zJuarez <mji2000.741@gmail.com>
2021-02-26 14:27:41 -0600

    centered text mision vision on sm

--- src/components/Home/HomeMisionVision/HomeMisionVision.js
+++ src/components/Home/HomeMisionVision/HomeMisionVision.js
@@ -6,25 +6,21 @@ const HomeMisionVision = () => (
   <div className="home-mision-vision-container">
     <div className="home-mision row">
       <div className="mision-overlay row">
-        <div className="col-sm-7">
-          <div className="text-mision">
+        <div className="col-sm-7 text-mision">
             <h1>Mission </h1>
             <p>
               Create a community that integrates students, teachers, alumni,
               companies, institutions and society in general to promote the
               development of robotics in Mexico.
             </p>
-          </div>
         </div>
       </div>
     </div>
     <div className="home-vision row">
       <div className="vision-overlay row">
-        <div className="col-sm-7 offset-sm-5">
-          <div className="text-vision">
+        <div className="col-sm-7 offset-sm-5 text-vision">
             <h1> Vision </h1>
             <p> Inspire future generations of the robotics community in Latin&nbsp;America.</p>
-          </div>
         </div>
       </div>
 
