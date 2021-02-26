f820a9a6988f1fb8e3ab294c4e2d052810f1c36c
Aurora Tijerina <auro.tj@gmail.com>
2021-02-26 11:56:11 -0600

    Added more content images for carousel and fixed some eslint errors

--- src/components/Home/HomeCarousel/HomeCarousel.js
+++ src/components/Home/HomeCarousel/HomeCarousel.js
@@ -3,55 +3,53 @@ import React, { Component } from 'react'
 import Carousel from 'react-material-ui-carousel'
 import './HomeCarousel.css'
 
-
-class HomeCarousel extends Component  {
-
-    /**
+class HomeCarousel extends Component {
+  /**
   * Parses path to projects image
   * @param {string} imgPath: Path to project's image/photo.
   * @return {path}
   */
  tryRequire = (imgPath: string) => {
-      try {
-        /* eslint-disable import/no-dynamic-require */
-        /* eslint-disable global-require */
-        return require(`../../../images/home/carousel/${imgPath}`)
-      } catch (err) {
-        return imgPath
-      }
-    }
+   try {
+     /* eslint-disable import/no-dynamic-require */
+     /* eslint-disable global-require */
+     return require(`../../../images/home/carousel/${imgPath}`)
+   } catch (err) {
+     return imgPath
+   }
+ }
 
-
-  /**
+ /**
    * Renders Responsive view of Projects's carousels.
    * @return {renderized_component} Home Carrousel only images
    */
-  render() {
-        const images = [];
-        const imagesNumber = 14;
+ render() {
+   const images = []
+   //  TODO [content]: Define content images
+   const imagesNumber = 17
 
-        for(let i = 1; i<=imagesNumber; i++){
-          images.push(i.toString())
-        }
+   for (let i = 1; i <= imagesNumber; i += 1) {
+     images.push(i.toString())
+   }
 
-    return (
-      <Carousel
-      interval={4000}
-      animation="slide"
-      autoPlay={true}
-      className="home-carousel"
-      indicators={false}
-      >
-          {
+   return (
+     <Carousel
+       interval={4000}
+       animation="slide"
+       autoPlay
+       className="home-carousel"
+       indicators={false}
+     >
+       {
             images.map((img, index) => (
-                  <div className="card-image-container "> 
-                        <img  src={this.tryRequire(`${img}.jpg`)} alt={index} />
-                  </div>
+              <div className="card-image-container ">
+                <img src={this.tryRequire(`${img}.jpg`)} alt={index} />
+              </div>
             ))
           }
-      </Carousel>
-    )
-  }
+     </Carousel>
+   )
+ }
 }
 
 export default HomeCarousel
