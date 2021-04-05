f276b431abfb4438c07029117a36a5ff7b1fb09a
Josecisneros001 <joseacisnerosm@gmail.com>
2021-04-04 20:33:04 -0500

    add mail sender code and templates

---
+++ public/mail/templates/join_request_response.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
+  </head>
+  <body>
+    <p>
+      Recibimos tu aplicación a <?= $_POST["position"] ?>
+    </p>
+    <p>
+      Evaluaremos tu solicitud de inmediato y te contactaremos en caso de requerir más información sobre ti <?= $_POST["from_name"] ?>.
+    </p>
+    <p>
+      Suerte!
+    </p>
+  </body>
+</html>
