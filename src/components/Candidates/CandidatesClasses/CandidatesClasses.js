eb9febb4b148215e8243415191ffb8c559194437
Aurora Tijerina <auro.tj@gmail.com>
2021-06-11 11:42:09 -0500

    FInished responsivity of playlists

--- src/components/Candidates/CandidatesClasses/CandidatesClasses.js
+++ src/components/Candidates/CandidatesClasses/CandidatesClasses.js
@@ -11,25 +11,26 @@ function CandidatesClasses() {
         container
         className="justify-content-md-center classes-main-row"
         spacing={2}
-        // justify="space-around"
         alignItems="center"
       >
-        <Grid item xs={4} className="classes-section-alignment">
+        <Grid item sm={12} md={4} className="classes-section-alignment">
           <div>
             <h className="learn-with-us">
               Learn with us on
             </h>
+            <br />
             <img
               className="youtube-logo"
               src={youtubeLogo}
               alt="youtube-logo"
             />
+            <br />
             <p className="caption-phrase">
               And be prepared to take the challenge!
             </p>
           </div>
         </Grid>
-        <Grid item xs={5} className="classes-section-alignment">
+        <Grid item sm={12} md={5} className="classes-section-alignment">
           <iframe
             className="central-playlist"
             src="https://www.youtube.com/embed/videoseries?list=PLbuFimxJ80Ynff7bdQjdIO7uKH1vZ-Jne"
@@ -39,26 +40,39 @@ function CandidatesClasses() {
             allowFullScreen
           />
         </Grid>
-        <Grid item xs={3}>
-          <Grid item xs={12}>
-            <iframe
-              className="secondary-playlist"
-              src="https://www.youtube.com/embed/videoseries?list=PLbuFimxJ80Yn97HhHeuvBvN47Z7cGHAI9"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <iframe
-              className="secondary-playlist"
-              src="https://www.youtube.com/embed/videoseries?list=PLbuFimxJ80YksGyBqJRjT6_nY0yWKpuzI"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
+        <Grid item sm={12} md={3}>
+          <Grid
+            container
+            className="justify-content-md-center classes-main-row"
+            spacing={2}
+            // justify="space-around"
+            alignItems="center"
+          >
+            <Grid item xs={6} sm={6} md={12}>
+              <iframe
+                className="secondary-playlist"
+                src="https://www.youtube.com/embed/videoseries?list=PLbuFimxJ80Yn97HhHeuvBvN47Z7cGHAI9"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </Grid>
+            <Grid item xs={6} sm={6} md={12}>
+              <iframe
+                className="secondary-playlist"
+                src="https://www.youtube.com/embed/videoseries?list=PLbuFimxJ80YksGyBqJRjT6_nY0yWKpuzI"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </Grid>
+            <Grid xs={12}>
+              <p className="caption-phrase-small">
+                And be prepared to take the challenge!
+              </p>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
