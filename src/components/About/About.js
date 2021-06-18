0a14c9f802a08214a8276689fe5016ddb3fac055
AldoSamaniego <a00825779@itesm.mx>
2021-06-18 15:52:36 -0500

    correct eslint

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,14 @@
+// @flow
 import React from 'react'
 import AboutTimeline from './AboutTimeline/AboutTimeline'
 import HeaderBanner from '../Shared/HeaderBanner/HeaderBanner'
 import './About.css'
 import Carousel from './AboutCarousel/carousel'
-import AboutHeader from './AboutHeader/AboutHeader'
 import RoborregosVideo from './VideoPlayer/Responsive-Video-Player'
 
 const About = () => {
   const headerTitle = 'About us'
-  const headerMainText = ['In 2015, four students organized themselves to compete in the Mexican National Robotics Tournament, reaching a pass to the international. There, they met people from other countries and noticed the potential that Mexico could have.','Motivated, they started RoBorregos, with the intention of sharing knowledge and experiences through generations.']
+  const headerMainText = ['In 2015, four students organized themselves to compete in the Mexican National Robotics Tournament, reaching a pass to the international. There, they met people from other countries and noticed the potential that Mexico could have.', 'Motivated, they started RoBorregos, with the intention of sharing knowledge and experiences through generations.']
   const headerSubText = ['Our journey']
   document.title = 'RoBorregos | About'
   return (
