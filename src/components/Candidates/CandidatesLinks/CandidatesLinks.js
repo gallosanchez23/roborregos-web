a614cfcab556507dd7c482eb2afce41523cc4e0c
Aurora Tijerina <auro.tj@gmail.com>
2021-06-18 14:55:33 -0500

    Fixed responsivity of social media message us section

--- src/components/Candidates/CandidatesLinks/CandidatesLinks.js
+++ src/components/Candidates/CandidatesLinks/CandidatesLinks.js
@@ -8,64 +8,45 @@ import instagramLogo from '../../../images/candidates/instagram-logo.png'
 
 function CandidatesLinks() {
   return (
-    <div className="images">
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
-        <Grid item sm={12} className="classes-section-alignment">
-          <div>
-            <h className="message-us">
-              Message Us!
-            </h>
-          </div>
-        </Grid>
-        <Grid item sm={4} className="center-logo">
-          <br />
-          <img
-            className="facebook-logo"
-            src={facebookLogo}
-            alt="facebook-logo"
-          />
-          <br />
-        </Grid>
-        <Grid item sm={4} className="center-logo">
-          <br />
-          <img
-            className="roborregos-logo"
-            src={roborregosLogo}
-            alt="RoBorregos-logo"
-          />
-          <br />
-        </Grid>
-        <Grid item sm={4} className="center-logo">
-          <br />
-          <img
-            className="instagram-logo"
-            src={instagramLogo}
-            alt="instagram-logo"
-          />
-          <br />
-        </Grid>
-        <Grid item sm={4} className="center-logo">
-          <div>
-            <h className="caption">
-              RoBorregos
-            </h>
-          </div>
-        </Grid>
-        <Grid item sm={4} className="center-logo" />
-        <Grid item sm={4} className="center-logo">
-          <div>
-            <h className="caption">
-              @roborregos
-            </h>
-          </div>
-        </Grid>
+    <Grid
+      container
+      alignItems="flex-start"
+      className="social-media"
+      justify="center"
+    >
+      <Grid item xs={12} className="message-us">
+        <h> Message Us! </h>
       </Grid>
-    </div>
+      <Grid item xs={12} md={4} onClick={() => window.open('https://www.facebook.com/RoBorregos/')}>
+        <img
+          className="social-media-logo"
+          src={facebookLogo}
+          alt="facebook-logo"
+        />
+        <br />
+        <h className="caption"> RoBorregos </h>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <br />
+        <img
+          className="roborregos-logo"
+          src={roborregosLogo}
+          alt="RoBorregos-logo"
+        />
+        <br />
+      </Grid>
+      <Grid item xs={12} md={4} onClick={() => window.open('https://www.instagram.com/roborregos/')}>
+        <img
+          className="social-media-logo"
+          src={instagramLogo}
+          alt="instagram-logo"
+        />
+        <br />
+        <h className="caption">
+          @roborregos
+        </h>
+      </Grid>
+    </Grid>
   )
 }
 
