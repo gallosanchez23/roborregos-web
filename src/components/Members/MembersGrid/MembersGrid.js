b790418580f6d20ece05333ffa148f944a09df0c
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-02-07 18:06:15 -0600

    feat: member with query params

--- src/components/Members/MembersGrid/MembersGrid.js
+++ src/components/Members/MembersGrid/MembersGrid.js
@@ -1,17 +1,23 @@
-import React, { useEffect, useState, useRef } from 'react'
-import Carousel from 'react-material-ui-carousel'
-import GridList from '@material-ui/core/GridList'
-import GridListTile from '@material-ui/core/GridListTile'
-import { Modal } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect, useState, useRef } from "react";
+import Carousel from "react-material-ui-carousel";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import { Modal } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCog, faCode, faBullhorn, faMicrochip, faRocket, faSearch,
-} from '@fortawesome/free-solid-svg-icons'
-import membersData from '../../../data/members.json'
-import placeholder from '../../../images/placeholder-rectangle.png'
-import { LARGE_WIDTH, MEDIUM_WIDTH, MOBILE_WIDTH } from '../../../constants'
-import MemberModal from './MemberModal/MemberModal'
-import './MembersGrid.css'
+  faCog,
+  faCode,
+  faBullhorn,
+  faMicrochip,
+  faRocket,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import membersData from "../../../data/members.json";
+import placeholder from "../../../images/placeholder-rectangle.png";
+import { LARGE_WIDTH, MEDIUM_WIDTH, MOBILE_WIDTH } from "../../../constants";
+import MemberModal from "./MemberModal/MemberModal";
+import { useLocation } from "react-router-dom";
+import "./MembersGrid.css";
 
 type Member = {
   id: number,
@@ -26,154 +32,188 @@ type Member = {
   semesters: string,
   subtitle: string,
   status: string,
-  role: string
+  role: string,
 };
 
-const inactiveMembers = membersData.members.filter((member) => member.status === 'inactive')
-const activeMembers = membersData.members.filter((member) => member.status === 'active' || member.status === 'comitee')
+const inactiveMembers = membersData.members.filter(
+  (member) => member.status === "inactive"
+);
+const activeMembers = membersData.members.filter(
+  (member) => member.status === "active" || member.status === "comitee"
+);
 
 const numberOfGridCols = () => {
   if (window.innerWidth >= LARGE_WIDTH) {
-    return 5
+    return 5;
   }
   if (window.innerWidth >= MEDIUM_WIDTH) {
-    return 4
+    return 4;
   }
   if (window.innerWidth >= MOBILE_WIDTH) {
-    return 3
+    return 3;
   }
-  return 2
-}
+  return 2;
+};
 
 const tryRequire = (imgPath: string) => {
   try {
-    return require(`../../../images/members/${imgPath}`)
+    return require(`../../../images/members/${imgPath}`);
   } catch (err) {
-    return placeholder
+    return placeholder;
   }
-}
+};
 
 function MembersGrid() {
-  const [showModal, setShowModal] = useState(false)
-  const [memberIndex, _setMemberIndex] = useState(0)
-  const [active, setActive] = useState(true)
-  const [gridCols, setGridCols] = useState(numberOfGridCols())
-  const [searchBarText, setSearchBarText] = useState('')
-  const [filteredMembers, setFilteredMembers] = useState([])
+  const [showModal, setShowModal] = useState(false);
+  const [firstLoad, setFirstLoad] = useState(false);
+  const [memberIndex, _setMemberIndex] = useState(0);
+  const [active, setActive] = useState(true);
+  const [gridCols, setGridCols] = useState(numberOfGridCols());
 
-  const extendKeywordSearch = (keywords: Array<string>, index: number, field: string) => {
-    let i = index
+  const queryParams = new URLSearchParams(location.search);
+
+  const param1 = queryParams.get("name");
+
+  const [searchBarText, setSearchBarText] = useState(param1 ?? "");
+  const [filteredMembers, setFilteredMembers] = useState([]);
+
+  const extendKeywordSearch = (
+    keywords: Array<string>,
+    index: number,
+    field: string
+  ) => {
+    let i = index;
     if (i + 1 < keywords.length) {
-      let j = i + 1
-      let substr = `${keywords[i]} ${keywords[j]}`
+      let j = i + 1;
+      let substr = `${keywords[i]} ${keywords[j]}`;
       while (j < keywords.length && field.toLowerCase().startsWith(substr)) {
-        substr += ` ${keywords[j]}`
-        j += 1
+        substr += ` ${keywords[j]}`;
+        j += 1;
       }
-      i = j - 1
+      i = j - 1;
     }
-    return i
-  }
+    return i;
+  };
 
   useEffect(() => {
-    const keywords = searchBarText.toLowerCase().split(' ')
-    setFilteredMembers(membersData.members.filter((member) => {
-      let name = true; let lastname = true; let role = true
-      for (let i = 0; i < keywords.length; i += 1) {
-        if (name && member.name.toLowerCase().startsWith(keywords[i])) {
-          i = extendKeywordSearch(keywords, i, member.name)
-          name = false
-        } else if (lastname && member.lastname.toLowerCase().startsWith(keywords[i])) {
-          i = extendKeywordSearch(keywords, i, member.lastname)
-          lastname = false
-        } else if (role && member.role.toLowerCase().startsWith(keywords[i])) {
-          i = extendKeywordSearch(keywords, i, member.role)
-          role = false
-        } else {
-          const tags = member.tags.toLowerCase().split(', ')
-          let matchTag = false
-          let k = i
-          for (let j = 0; j < tags.length; j += 1) {
-            if (tags[j].startsWith(keywords[i])) {
-              k = Math.max(extendKeywordSearch(keywords, i, tags[j]), k)
-              matchTag = true
+    const keywords = searchBarText.toLowerCase().split(" ");
+    setFilteredMembers(
+      membersData.members.filter((member) => {
+        let name = true;
+        let lastname = true;
+        let role = true;
+        for (let i = 0; i < keywords.length; i += 1) {
+          if (name && member.name.toLowerCase().startsWith(keywords[i])) {
+            i = extendKeywordSearch(keywords, i, member.name);
+            name = false;
+          } else if (
+            lastname &&
+            member.lastname.toLowerCase().startsWith(keywords[i])
+          ) {
+            i = extendKeywordSearch(keywords, i, member.lastname);
+            lastname = false;
+          } else if (
+            role &&
+            member.role.toLowerCase().startsWith(keywords[i])
+          ) {
+            i = extendKeywordSearch(keywords, i, member.role);
+            role = false;
+          } else {
+            const tags = member.tags.toLowerCase().split(", ");
+            let matchTag = false;
+            let k = i;
+            for (let j = 0; j < tags.length; j += 1) {
+              if (tags[j].startsWith(keywords[i])) {
+                k = Math.max(extendKeywordSearch(keywords, i, tags[j]), k);
+                matchTag = true;
+              }
+            }
+            i = k;
+            if (!matchTag) {
+              return false;
             }
           }
-          i = k
-          if (!matchTag) {
-            return false
-          }
         }
-      }
-      return true
-    }))
-  }, [searchBarText])
+        return true;
+      })
+    );
+  }, [searchBarText]);
 
-  const memberIndexRef = useRef(memberIndex)
+  const memberIndexRef = useRef(memberIndex);
 
   const setMemberIndex = (index) => {
-    memberIndexRef.current = index
-    _setMemberIndex(index)
-  }
+    memberIndexRef.current = index;
+    _setMemberIndex(index);
+  };
 
   const memberList = (() => {
-    if (searchBarText !== '') {
-      return filteredMembers
+    if (searchBarText !== "") {
+      return filteredMembers;
     }
-    return active ? activeMembers : inactiveMembers
-  })()
+    return active ? activeMembers : inactiveMembers;
+  })();
+
+  useEffect(() => {
+    if (param1 && filteredMembers.length > 0 && !firstLoad) {
+      setShowModal(true);
+      setMemberIndex(filteredMembers[0].index);
+      setActive(filteredMembers[0].status !== "inactive");
+      setFirstLoad(true);
+    }
+  }, [param1, filteredMembers]);
 
   useEffect(() => {
     const updateGridCols = () => {
-      setGridCols(numberOfGridCols())
-    }
+      setGridCols(numberOfGridCols());
+    };
 
     const keyListener = (event: KeyboardEvent) => {
       if (event.keyCode === 27) {
-        setShowModal(false)
+        setShowModal(false);
       }
       if (event.keyCode === 37) {
         if (memberIndexRef.current === 0) {
-          setMemberIndex(memberList.length - 1)
+          setMemberIndex(memberList.length - 1);
         } else {
-          setMemberIndex(memberIndexRef.current - 1)
+          setMemberIndex(memberIndexRef.current - 1);
         }
       } else if (event.keyCode === 39) {
-        setMemberIndex((memberIndexRef.current + 1) % memberList.length)
+        setMemberIndex((memberIndexRef.current + 1) % memberList.length);
       }
-    }
+    };
 
-    window.addEventListener('resize', updateGridCols)
-    document.addEventListener('keydown', keyListener, false)
+    window.addEventListener("resize", updateGridCols);
+    document.addEventListener("keydown", keyListener, false);
 
     return () => {
-      window.removeEventListener('resize', updateGridCols)
-      document.removeEventListener('keydown', keyListener)
-    }
-  }, [memberList])
+      window.removeEventListener("resize", updateGridCols);
+      document.removeEventListener("keydown", keyListener);
+    };
+  }, [memberList]);
 
   const memberIcon = (role: string) => {
-    if (role === 'Software Development') {
-      return faCode
-    } if (role === 'Electronics') {
-      return faMicrochip
-    } if (role === 'Mechanical Design') {
-      return faCog
-    } if (role === 'Logistics & Sponsorship') {
-      return faBullhorn
+    if (role === "Software Development") {
+      return faCode;
     }
-    return faRocket
-  }
+    if (role === "Electronics") {
+      return faMicrochip;
+    }
+    if (role === "Mechanical Design") {
+      return faCog;
+    }
+    if (role === "Logistics & Sponsorship") {
+      return faBullhorn;
+    }
+    return faRocket;
+  };
 
   const renderGridList = (members: Array<Member>, title: string) => (
     <div>
       <div
-        style={{ display: (title === '') ? 'none' : 'block' }}
+        style={{ display: title === "" ? "none" : "block" }}
         className="grid-title"
       >
-        <h1 className="grid-title-text">
-          { title }
-        </h1>
+        <h1 className="grid-title-text">{title}</h1>
       </div>
       <GridList
         cellHeight="auto"
@@ -181,16 +221,16 @@ function MembersGrid() {
         cols={gridCols}
         spacing={3}
       >
-        { members.map((member, index) => (
+        {members.map((member, index) => (
           <GridListTile
             key={member.id}
             cols={1}
             id={`members-grid-tile-${member.id}`}
             className="members-grid-tile"
             onClick={() => {
-              setShowModal(true)
-              setMemberIndex(index)
-              setActive(member.status !== 'inactive')
+              setShowModal(true);
+              setMemberIndex(index);
+              setActive(member.status !== "inactive");
             }}
           >
             <div className="member-image-container">
@@ -200,9 +240,7 @@ function MembersGrid() {
                 alt={member.name}
               />
               <div className="member-image-content">
-                <p>
-                  { `${member.name} ${member.lastname}` }
-                </p>
+                <p>{`${member.name} ${member.lastname}`}</p>
                 <div className="member-image-icon">
                   <FontAwesomeIcon
                     icon={memberIcon(member.role)}
@@ -213,13 +251,16 @@ function MembersGrid() {
               </div>
             </div>
           </GridListTile>
-        )) }
+        ))}
       </GridList>
     </div>
-  )
+  );
 
   return (
-    <div className="members-grid-container" data-testid="members-grid-container">
+    <div
+      className="members-grid-container"
+      data-testid="members-grid-container"
+    >
       <div className="members-grid-search-bar">
         <FontAwesomeIcon icon={faSearch} className="members-search-bar-icon" />
         <input
@@ -230,9 +271,12 @@ function MembersGrid() {
           value={searchBarText}
         />
       </div>
-      { searchBarText === '' ? (
-        [renderGridList(activeMembers, ''), renderGridList(inactiveMembers, 'RoBorregos Legacy')]
-      ) : renderGridList(filteredMembers, '')}
+      {searchBarText === ""
+        ? [
+            renderGridList(activeMembers, ""),
+            renderGridList(inactiveMembers, "RoBorregos Legacy"),
+          ]
+        : renderGridList(filteredMembers, "")}
       <Modal
         className="modal-container"
         data-testid="modal-container"
@@ -248,20 +292,18 @@ function MembersGrid() {
             index={memberIndex}
             startAt={memberIndex}
           >
-            {
-              memberList.map((member) => (
-                <MemberModal
-                  member={member}
-                  onHide={() => setShowModal(false)}
-                  key={member.id}
-                />
-              ))
-            }
+            {memberList.map((member) => (
+              <MemberModal
+                member={member}
+                onHide={() => setShowModal(false)}
+                key={member.id}
+              />
+            ))}
           </Carousel>
         )}
       />
     </div>
-  )
+  );
 }
 
-export default MembersGrid
+export default MembersGrid;
